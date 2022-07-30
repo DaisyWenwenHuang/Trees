256c174c2c7fbd6d44de4705c0b265db8c9aaf50
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-07-30 05:06:47 +0000

    done

--- balancetree.py
+++ balancetree.py
@@ -1,3 +1,24 @@
 # https://leetcode.com/problems/balanced-binary-tree/
 # leetcode 110 easy
 # height balaled tree :a binary tree in which the left and right subtrees of every node differ in height by no more than 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root):
+			# edge case
+            if not root:
+                return [True,0]
+            
+            left,right = dfs(root.left),dfs(root.right)
+            
+            balanced = (left[0]) and right[0] and (abs(left[1]-right[1])<=1)
+            height = 1 + max(left[1],right[1])
+            return [balanced,height]
+        
+        return dfs(root)[0]
