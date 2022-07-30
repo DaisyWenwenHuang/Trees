6bbc5a2b2909790c7e6bdabc61b86ee1e73fb643
TheVeryHungryCoderpillar <47543499+DaisyWenwenHuang@users.noreply.github.com>
2022-07-30 04:48:08 +0000

    update

--- balancetree.py
+++ balancetree.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/balanced-binary-tree/
 # leetcode 110 easy
+# height balaled tree :a binary tree in which the left and right subtrees of every node differ in height by no more than 1
